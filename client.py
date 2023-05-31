7688f66559370197428c926dad8b8a444e8a4d5c
Mohamed Elgendy <93053571+gendyvip@users.noreply.github.com>
2023-05-31 06:30:33 +0300

    TimeServer

---
+++ client.py
@@ -0,0 +1,16 @@
+from socket import *
+try:
+    s=socket(AF_INET, SOCK_STREAM)
+    host="127.0.0.1"
+    port=7002
+    s.connect((host,port))
+    while True:
+        x=s.recv(2048) 
+        print("server : ",x.decode('utf-8'))
+    s.close()
+except error as e:
+    print(e)
+except KeyboardInterrupt :
+    print("chat is terminated") 
+
+
